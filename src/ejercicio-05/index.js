90ac8c8f0d3fbce6fe5d0276caeee57896b25dfd
Blanky_9923 <blankita77@gmail.com>
2024-04-23 14:25:34 +0200

    Añadir ejercicio-05

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+const product= {
+    name: 'Smartphone',
+    price: 400,
+    category: 'Electronics'
+}
+const{name,price,category } = product
+
+console.log(`Nombre: ${name}`)
+console.log(`Precio: ${price}`)
+console.log(`Categoria: ${category}`)
