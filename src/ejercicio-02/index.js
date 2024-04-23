310d417d0ef1006335247bb6412ab41c54155353
Blanky_9923 <blankita77@gmail.com>
2024-04-23 13:52:20 +0200

    Añadir ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,16 @@
 // Escribe tu código aquí:
+const person = {
+    name : 'Jhon',
+    greet : function () {
+      console.log(`Hola, soy ${this.name}`)
+    }
+} 
+person.greet()
+
+const person1= {
+    name:'Marta',
+    greet: () => { 
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+person1.greet()
