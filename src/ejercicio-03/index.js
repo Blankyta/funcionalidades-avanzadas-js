e723e22a3c605518ccfcc18dfa15fcee22bc45ba
Blanky_9923 <blankita77@gmail.com>
2024-04-23 14:03:27 +0200

    Añadir ejercicio-03

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+const person = {
+    name:'Blanca',
+    age: 46,
+    city: 'Burgos'
+}
+const message = `Hola, me llamo ${person.name}, tengo ${person.age}, y vivo en ${person.city}.`
+console.log(message)
