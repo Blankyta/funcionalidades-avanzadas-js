76bb2a5a3f924ee4e13437e1af85c7376646e995
Blanky_9923 <blankita77@gmail.com>
2024-04-25 02:34:11 +0200

    Añadir ejercicio-10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí:
+const personas = new Map()
+
+personas.set('Blanca', 46)
+personas.set('Angel', 45)
+personas.set('Alejandro', 4)
+personas.set('Daniela', 10)
+
+console.log('Edad de Alejandro es: ', personas.get('Alejandro'))
+
+const personToRemove = 'Angel'
+personas.delete(personToRemove)
+
+console.log('Personas en el Map despues de eliminar a Angel:', personas)
